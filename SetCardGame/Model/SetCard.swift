5da07a2826b99c77a39c57e218cf6225b9d3a716
Илья Абдуллин <dejavue66@gmail.com>
2019-02-11 14:29:46 +0300

    Add setCard model.

---
+++ SetCardGame/Model/SetCard.swift
@@ -0,0 +1,46 @@
+//
+//  setCard.swift
+//  SetCardGame
+//
+//  Created by Ilya Abdullin on 08/02/2019.
+//  Copyright © 2019 Ilya Abdullin. All rights reserved.
+//
+
+import Foundation
+
+struct SetCard: Equatable {
+    let symbol: Symbol
+    let number: Number
+    let shading: Shading
+    let color: Filling
+    
+    enum Symbol: String, CaseIterable {
+        case triangle = "▲"
+        case square = "■"
+        case circle = "●"
+        
+        static var all = [Symbol.triangle, .square, .circle]
+    }
+    
+    enum Number: Int, CaseIterable {
+        case one = 1, two, three
+        
+        static var all = [Number.one, .two, .three]
+    }
+    
+    enum Shading: CaseIterable {
+        case solid
+        case striped
+        case open
+        
+        static var all = [Shading.solid, .striped, .open]
+    }
+    
+    enum Filling: Int, CaseIterable {
+        case red = 0xFF3B30
+        case green = 0x4CD964
+        case purple = 0x5856D6
+        
+        static var all = [Filling.red, .green, .purple]
+    }
+}
